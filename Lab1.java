28eb8367d16afbce48b53d2d99230ba21b944c40
innocentmugaga2-boop <innocentmugaga2@gmail.com>
2026-09-05 16:23:40 +0300

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,25 @@
+public class Lab1{
+    
+    public static void main(String[]args) {
+        // University student;
+        int Age =20;
+        char Gender ='M';
+        String  Name ="INNOCENT";
+        double CGPA =4.8;
+        Boolean paidTuition =true;
+        long S_Contact=704028093;
+        short roomNumber= 214;
+        byte retakeCount =2;
+        float attendancepercent=70.5f;
+        System.out.println("Student's profile");
+        System.out.println("Age:"+Age);
+        System.out.println("Gender:"+ Gender);
+        System.out.println("Name" + Name);
+        System.out.println("CGPA:"+CGPA);
+        System.out.println("paidtuition:"+paidTuition);
+        System.out.println("S_Contact:"+S_Contact);
+        System.out.println("roomNumber:"+roomNumber);
+        System.out.println("retakeCount:"+retakeCount);
+        System.out.println("attendancepercent:"+attendancepercent);
+    }
+}
---
+++ Snippet1.java
@@ -0,0 +1,14 @@
+
+    public class Snippet1 {
+    public static void main(String[] args) {
+
+        final int MAX_RETAKES = 3;
+
+        System.out.println("Retakes allowed: " + MAX_RETAKES);
+
+        // MAX_RETAKES cannot be changed because it is final
+
+        System.out.println("Retakes allowed: " + MAX_RETAKES);
+    }
+}
+
